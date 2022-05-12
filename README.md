23c62a332c658cb29e9b33384ba988497ad31285
Alejandro Gómez <sagomeza@correo.udistrital.edu.co>
2022-05-12 14:31:57 -0500

    readme

---
+++ README.md
@@ -0,0 +1,1 @@
+EDA sobre un dataset que registra cerca de 20000 partidas jugadas en lichess.org
---
+++ ejec.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+git remote add origin git@github.com:alejogomez1/EDA_dataset_lichess.git
+git branch -M main
+git push -u origin main
